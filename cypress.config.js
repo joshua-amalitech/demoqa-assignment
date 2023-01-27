7ad811275ee11bac7fcc0b2df54625cb47475f99
Joshua Tetteh <joshua.tetteh@amalitech.com>
2023-01-27 16:36:27 +0000

    Working on Text Box Field

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    // excludeSpecPattern: "**/e2e/text_box_page.cy.js",
+    baseUrl: "https://demoqa.com",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
--- cypress/e2e/text_box_page.cy.js
+++ cypress/e2e/text_box_page.cy.js
@@ -1,5 +1,30 @@
-describe('Testing Text Box Page', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+describe('Text Box Page Test', () => {
+
+  before(() => {
+    cy.visit("/text-box");
   })
+
+  it("Fill in input fields", () => {
+    cy.get("#userName")
+      .should("be.visible")
+      .type("Test Name")
+      .should('contain.value', 'Test Name');
+
+    cy.get("#userEmail")
+      .should("be.visible")
+      .type("test@gmail.com")
+      .should('contain.value', 'test@gmail.com');
+
+    cy.get("#currentAddress")
+      .should("be.visible")
+      .type("P.O.BOX Address")
+      .should('contain.value', 'P.O.BOX Address');
+
+    cy.get("#permanentAddress")
+      .should("be.visible")
+      .type("P.O.BOX Address")
+      .should('contain.value', 'P.O.BOX Address');
+  })
+
 })
