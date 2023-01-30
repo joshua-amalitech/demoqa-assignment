bd6900959f70a02fd9e5c5d2dbccc65ff8ea7879
Joshua Tetteh <joshua.tetteh@amalitech.com>
2023-01-30 15:43:28 +0000

    Testing out Textbox page 2

--- cypress/e2e/text_box_page.cy.js
+++ cypress/e2e/text_box_page.cy.js
@@ -1,6 +1,5 @@
 
 describe('Text Box Page Test', () => {
-
   let user = {}
 
   beforeEach(() => {
@@ -41,35 +40,35 @@ describe('Text Box Page Test', () => {
     cy.get("#output #permanentAddress").should('contain.text', user.permanent_address)
   })
 
-  it("Fill in input fields", () => {
-    cy.get("#userName")
-      .should("be.visible")
-      .type(user.name)
-      .should('contain.value', user.name);
+  // it("Fill in input fields", () => {
+  //   cy.get("#userName")
+  //     .should("be.visible")
+  //     .type(user.name)
+  //     .should('contain.value', user.name);
 
-    cy.get("#userEmail")
-      .should("be.visible")
-      .type(user.email)
-      .should('contain.value', user.email);
+  //   cy.get("#userEmail")
+  //     .should("be.visible")
+  //     .type(user.email)
+  //     .should('contain.value', user.email);
 
-    cy.get("#currentAddress")
-      .should("be.visible")
-      .type(user.current_address)
-      .should('contain.value', user.current_address);
+  //   cy.get("#currentAddress")
+  //     .should("be.visible")
+  //     .type(user.current_address)
+  //     .should('contain.value', user.current_address);
 
-    cy.get("#permanentAddress")
-      .should("be.visible")
-      .type(user.permanent_address)
-      .should('contain.value', user.permanent_address);
+  //   cy.get("#permanentAddress")
+  //     .should("be.visible")
+  //     .type(user.permanent_address)
+  //     .should('contain.value', user.permanent_address);
 
-    cy.get('#submit')
-      .should("be.visible")
-      .click();
+  //   cy.get('#submit')
+  //     .should("be.visible")
+  //     .click();
 
-    cy.get("#output #name").should('contain.text', user.name);
-    cy.get("#output #email").should('contain.text', user.email)
-    cy.get("#output #currentAddress").should('contain.text', user.current_address)
-    cy.get("#output #permanentAddress").should('contain.text', user.permanent_address)
-  })
+  //   cy.get("#output #name").should('contain.text', user.name);
+  //   cy.get("#output #email").should('contain.text', user.email)
+  //   cy.get("#output #currentAddress").should('contain.text', user.current_address)
+  //   cy.get("#output #permanentAddress").should('contain.text', user.permanent_address)
+  // })
 
 })
